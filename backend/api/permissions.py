1e22c77eaca8b87543396d561e791681b0331ccf
Alexandr <s2dent18@yandex.ru>
2021-10-01 09:58:51 +0500

    added PATCH

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -9,5 +9,4 @@ class ReadOnly(permissions.BasePermission):
 class AuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user and request.method in (
-                    'PUT', 'DELETE'))
+                or obj.author == request.user)
